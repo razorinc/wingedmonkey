ecace4321204ae385072acf130a337f02e5beef3
Jiri Tomasek <jtomasek@redhat.com>
2012-11-14 11:32:07 +0100

    mock for the new provider application form

--- src/app/controllers/provider_applications_controller.rb
+++ src/app/controllers/provider_applications_controller.rb
@@ -6,4 +6,13 @@ class ProviderApplicationsController < ApplicationController
       @provider_applications = current_provider.list_systems
     end
   end
+
+  def new
+    if params[:launchable_id].present?
+      @launchable = Launchable.find(params[:launchable_id])
+      @provider_application = ProviderApplication.new
+    else
+      redirect_to launchables_path, :alert => "Please select Application Blueprint first"
+    end
+  end
 end
