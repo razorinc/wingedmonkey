7c05c290fd4421c13a983c88d8cbb4aff0d8761b
Tzu-Mainn Chen <tzumainn@redhat.com>
2012-11-16 11:46:35 -0500

    do not require_provider when accessing provider select

--- src/app/controllers/providers_controller.rb
+++ src/app/controllers/providers_controller.rb
@@ -1,4 +1,6 @@
 class ProvidersController < ApplicationController
+  skip_before_filter :require_provider, :only => :select
+
   def select
     set_current_provider params[:provider]
     render :nothing => true
