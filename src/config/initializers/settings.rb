9dcbeff5d14b81375ee9c6245b6137704a063539
Jiri Tomasek <jtomasek@redhat.com>
2012-11-23 13:03:54 +0100

    removed settings as it is not used any more

--- src/config/initializers/settings.rb
+++
@@ -1,7 +0,0 @@
-# load the settings.yml file and make it available to the app
-require 'yaml'
-
-settings_file = File.join("#{Rails.root}", "config", "settings.yml")
-SETTINGS = YAML.load_file(settings_file) rescue {}
-
-# PROVIDERS = ProviderCollection.new(SETTINGS[:providers])
