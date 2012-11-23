02493414a40d6acefb151a1a940cd87657226813
Jiri Tomasek <jtomasek@redhat.com>
2012-11-23 14:01:51 +0100

    removed initialize methods from mock_launchable and mock_provider_application

--- src/app/models/providers/mock/mock_launchable.rb
+++ src/app/models/providers/mock/mock_launchable.rb
@@ -1,15 +1,6 @@
 module Providers
   module Mock
     class MockLaunchable < Launchable
-      def initialize config
-        @config = config
-        # create delegator methods to access configuration info
-        # but, don't expose credentials
-        @config.each.reject{|k,v| k == :credentials}.each do |k,v|
-          self.class.send(:define_method, k, proc {@config[k]})
-        end
-      end
-
       # List all of the launchable items.
       # The list may be reduced by the information in the current context.
       # For instance, the current user, the user's permissions, etc...
