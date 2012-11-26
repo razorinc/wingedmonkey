aa81b7cd4d0772608d887652894025af5694ac33
Tzu-Mainn Chen <tzumainn@redhat.com>
2012-11-26 15:18:24 -0500

    added in-memory 'applications' for mock provider applications

--- src/app/models/providers/mock/mock_provider_application.rb
+++ src/app/models/providers/mock/mock_provider_application.rb
@@ -1,10 +1,26 @@
 module Providers
   module Mock
     class MockProviderApplication < ProviderApplication
+      @@provider_applications = []
+      @@id = 0
+
+      def initialize(attributes = {})
+        attributes.each do |name, value|
+          send("#{name}=", value)
+        end
+        @id = @@id
+        @@id = @@id + 1
+        @@provider_applications << self
+      end
+
+      def launchable
+        Providers::Mock::MockLaunchable.find(@launchable_id)
+      end
+
       # List all of the Provider Applications.
       def self.all filter=nil
         filter ||= {}
-        {}
+        @@provider_applications
       end
     end
   end
