3d061370eba41243c6592e44e2e3ea2fe331c6b1
Greg Blomquist <gblomqui@redhat.com>
2012-11-29 16:14:42 -0500

    Removing Devise gem
    Since we're no longer authenticating users directly, we can drop devise.
    If we ever resurrect devise (or direct user authentication) we can either revert
    this or re-implement.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: "provider_applications#index"
 
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
-
   resources :launchables
   resources :provider_applications
   resources :sessions
