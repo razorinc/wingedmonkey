f49db8357f4a81323d57aa64431764a265be71c5
Greg Blomquist <gblomqui@redhat.com>
2012-12-05 12:55:03 -0500

    Move the mock provider_applications collection from mock_provider_application to mock_connection.
    The provider_applications should be accessed as part of the connection object
    instead of being housed internal to the mock_provider_application class.
    
    Also, this changes the mock_provider_appliaction to use the
    ProviderModel.connect! method to get a mock_connection.

--- src/app/models/providers/mock/mock_provider_application.rb
+++ src/app/models/providers/mock/mock_provider_application.rb
@@ -1,38 +1,31 @@
 module Providers
   module Mock
     class MockProviderApplication < ProviderApplication
-      @@provider_applications = []
-      @@id = 1
-
-      def initialize(attributes = {})
-        attributes.each do |name, value|
-          send("#{name}=", value)
-        end
-      end
-
       def launchable
-        Providers::Mock::MockLaunchable.find(@launchable_id)
+        Launchable.find(@launchable_id)
       end
 
       def save
-        @id = @@id
-        @@id = @@id + 1
         @state = 'running'
-        @@provider_applications << self
+        self.class.connect! do |connection|
+          connection.add_application(self)
+        end
       end
 
       def destroy
-        @@provider_applications.delete(self)
+        self.class.connect! do |connection|
+          connection.destroy_application(self)
+        end
       end
 
-      # List all of the Provider Applications.
       def self.all filter=nil
-        filter ||= {}
-        @@provider_applications
+        connect! {|connection| connection.applications}
       end
 
-      def self.find(id)
-        @@provider_applications.find{ |app| app.id.to_s == id }
+      def self.find id
+        connect! do |connection|
+          connection.applications.find{|app| app.id.to_s == id}
+        end
       end
     end
   end
