cd709ec1ac3ea04787bdd2c6ea0d4670ecd38cdd
Tzu-Mainn Chen <tzumainn@redhat.com>
2012-12-11 11:28:25 -0500

    initial openstack integration

--- app/controllers/provider_applications_controller.rb
+++ app/controllers/provider_applications_controller.rb
@@ -5,6 +5,7 @@ class ProviderApplicationsController < ApplicationController
 
   def new
     if params[:launchable_id].present?
+      @provider = current_provider
       @launchable = Launchable.find(params[:launchable_id])
       @provider_application = ProviderApplication.create({:launchable_id => @launchable.id})
     else
---
+++ app/models/providers/openstack.rb
@@ -0,0 +1,8 @@
+require 'providers/openstack/openstack_provider'
+require 'providers/openstack/openstack_provider_application'
+require 'providers/openstack/openstack_launchable'
+
+module Providers
+  module OpenStack
+  end
+end
