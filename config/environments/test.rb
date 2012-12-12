58eb32cfff2c5800effb57556285be4c3c95e9c7
Greg Blomquist <gblomqui@redhat.com>
2012-12-12 11:48:58 -0500

    Make the location of providers.yml configurable per environment.
    Also, add a default providers_test.yml for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ WingedMonkey::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.providers_file = File.join("#{Rails.root}", "config", "providers_test.yml")
 end
