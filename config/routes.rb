75bcc210aecfe30a564aacddbb73a415b74f3954
Jiri Tomasek <jtomasek@redhat.com>
2012-12-14 13:42:05 +0100

    Added provider_application detail page and launch summary as third step view of application launch workflow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "provider_applications#index"
 
   resources :launchables
-  resources :provider_applications
+  resources :provider_applications do
+    get 'launch_summary', :on => :member
+  end
 
   match 'login',       :to => 'sessions#new',     :as => 'login'
   match 'logout',      :to => 'sessions#destroy', :as => 'logout'
