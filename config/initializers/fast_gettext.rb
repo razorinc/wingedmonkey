8355e5061e8e8db9d14db7766c8eaec4c804f900
Jiri Tomasek <jtomasek@redhat.com>
2013-01-14 13:53:38 +0100

    moved gettext from locales folder because of yml issues, added non activerecord model attributes translation strings

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
-FastGettext.add_text_domain 'app', :path => 'config/locales/gettext', :type => :po
+FastGettext.add_text_domain 'app', :path => 'config/gettext_locales', :type => :po
 FastGettext.default_available_locales = ['en']
 FastGettext.default_text_domain = 'app'
