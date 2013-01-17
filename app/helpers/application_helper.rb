656710c669ffb8b84e89c897b98e7d713e133c7c
Tzu-Mainn Chen <tzumainn@redhat.com>
2013-01-17 16:48:40 -0500

    added seconds_to_time_string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,28 @@ module ApplicationHelper
     session[:current_provider_creds].present?
   end
 
+  def seconds_to_time_string(seconds)
+    if seconds
+      remaining_seconds = seconds.to_i
+      time_string = ""
+      if remaining_seconds > 86400
+        time_string += "#{remaining_seconds / 86400}d, "
+        remaining_seconds = remaining_seconds % 86400
+      end
+      if remaining_seconds > 3600
+        time_string += "#{remaining_seconds / 3600}h, "
+        remaining_seconds = remaining_seconds % 3600
+      end
+      if remaining_seconds > 60
+        time_string += "#{remaining_seconds / 60}m, "
+        remaining_seconds = remaining_seconds % 60
+      end
+      time_string += "#{remaining_seconds}s"
+    else
+      "N/A"
+    end
+  end
+
   private
   def provider_partial(partial_name)
     provider_name = current_provider.type
