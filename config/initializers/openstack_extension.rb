f69af56ee1515348bfad0986fc0be0374a40f59f
Tzu-Mainn Chen <tzumainn@redhat.com>
2013-01-17 14:32:58 -0500

    allow for ram to be in MB

--- config/initializers/openstack_extension.rb
+++ config/initializers/openstack_extension.rb
@@ -1,8 +1,12 @@
 require 'openstack'
 
 class OpenStack::Compute::Flavor
+  def ram_string
+    @ram >= 1024 ? "#{@ram/1024}GB" : "#{@ram}MB"
+  end
+
   def description
-    "#{@ram/1024}GB RAM | #{@vcpus} VCPU | #{@disk}GB Disk"
+    "#{ram_string} RAM | #{@vcpus} VCPU | #{@disk}GB Disk"
   end
 
   def name_with_description
