d2824fd92c02d035c8a41a9f3e2841a10e1424d1
Tzu-Mainn Chen <tzumainn@redhat.com>
2013-01-18 10:24:55 -0500

    use number_to_human_size instead of ad hoc ram formatting

--- config/initializers/openstack_extension.rb
+++ config/initializers/openstack_extension.rb
@@ -1,11 +1,8 @@
 require 'openstack'
 
 class OpenStack::Compute::Flavor
-  def ram_string
-    @ram >= 1024 ? "#{@ram/1024}GB" : "#{@ram}MB"
-  end
-
   def description
+    ram_string = ActionController::Base.helpers.number_to_human_size(@ram * 1024 * 1024)
     "#{ram_string} RAM | #{@vcpus} VCPU | #{@disk}GB Disk"
   end
 
