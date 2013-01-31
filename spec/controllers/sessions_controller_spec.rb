6d7579ad094eaeb65bff3a88f0bdeec90847dd5a
Tzu-Mainn Chen <tzumainn@redhat.com>
2013-01-31 15:48:37 -0500

    added missing tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,25 @@ describe SessionsController do
     end
   end
 
+  it "should create user session if valid credentials are provided" do
+    session[:current_provider_id] = "my_mock"
+    post :create, :params => { :username => "admin", :password => "password" }
+    response.should redirect_to(root_url)
+  end
+
+  it "should not create user session if invalid credentials are provided" do
+    session[:current_provider_id] = "test_conductor"
+    post :create, :params => { :username => "admin", :password => "password" }
+    response.should render_template("new")
+  end
+
+  describe "when logged in as mock_user" do
+    describe "GET 'destroy'" do
+      it "redirects to login" do
+        get 'destroy'
+        response.should redirect_to(login_path)
+      end
+    end
+  end
+
 end
