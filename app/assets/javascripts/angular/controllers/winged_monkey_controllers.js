51645d05b9f9168d29d3719033fb43acb3a4555c
Jiri Tomasek <jtomasek@redhat.com>
2013-02-07 19:12:53 +0100

    Provider applications list show/hide messages

--- app/assets/javascripts/angular/controllers/winged_monkey_controllers.js
+++ app/assets/javascripts/angular/controllers/winged_monkey_controllers.js
@@ -1,9 +1,12 @@
 var wingedMonkeyControllers = angular.module('wingedMonkeyControllers',[]);
 
-wingedMonkeyControllers.controller("ProviderAppsCtrl", function($scope, $filter, ProviderApplication) {
+wingedMonkeyControllers.controller("ProviderAppsCtrl", function($scope, $filter, ProviderApplication, $http) {
+  $scope.appsLoaded = false;
+
   $scope.refreshProviderApps = function() {
     ProviderApplication.query(function(data){
       $scope.providerApps = data;
+      $scope.appsLoaded = true;
     });
   };
 
