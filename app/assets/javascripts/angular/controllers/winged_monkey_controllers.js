2ada6c33fca5a88ae22411408b7f6e35fd309831
Jiri Tomasek <jtomasek@redhat.com>
2013-02-08 17:28:09 +0100

    Stop functionality wont remove the application untill the refresh occurs

--- app/assets/javascripts/angular/controllers/winged_monkey_controllers.js
+++ app/assets/javascripts/angular/controllers/winged_monkey_controllers.js
@@ -15,9 +15,11 @@ wingedMonkeyControllers.controller("ProviderAppsCtrl", function($scope, $filter,
   $scope.destroyProviderApp = function(app_id) {
     $scope.providerApps.forEach(function(app, index) {
       if (app_id === app.id) {
-        app.$delete({id: app.id}, function() {
+        // app.$delete({id: app.id}, function() {
+        ProviderApplication.delete({id: app.id}, function() {
           //success
-          $scope.providerApps.splice(index, 1);
+          app.state = "DELETING";
+          // $scope.providerApps.splice(index, 1);
         }, function(response){
           //failure
           console.log(response);
