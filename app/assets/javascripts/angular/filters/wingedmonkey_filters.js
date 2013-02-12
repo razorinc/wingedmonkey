8f6f1a4d7439659cd13efa123711a5e964a3f99f
Jiri Tomasek <jtomasek@redhat.com>
2013-02-12 13:15:32 +0100

    Polling support for Ovirt, added as_json, N/A filter, provider_application view

--- app/assets/javascripts/angular/filters/wingedmonkey_filters.js
+++ app/assets/javascripts/angular/filters/wingedmonkey_filters.js
@@ -7,3 +7,12 @@ wingedMonkeyFilters.filter('findById', function($filter) {
     }
   };
 });
+
+// replace undefined with N/A string (custom string is passed)
+wingedMonkeyFilters.filter('notAvailable', function($filter) {
+  return function(input, replacementString){
+    if(typeof input !== 'undefined'){
+      return replacementString;
+    }
+  };
+});
