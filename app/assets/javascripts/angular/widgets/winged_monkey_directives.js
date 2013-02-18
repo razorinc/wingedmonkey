4e398d264824e425d71b6ec6de4b7a9231cd4353
Jiri Tomasek <jtomasek@redhat.com>
2013-02-18 10:17:12 +0100

    Display confirm dialog when clicked on terminate button

--- app/assets/javascripts/angular/widgets/winged_monkey_directives.js
+++ app/assets/javascripts/angular/widgets/winged_monkey_directives.js
@@ -12,3 +12,44 @@ wingedMonkeyDirectives.directive('wmToggle', function () {
     });
   };
 });
+
+wingedMonkeyDirectives.directive('wmProviderAppConfirm', function() {
+  return{
+    restrict: "A",
+    replace: true,
+    scope: {
+      message: '@wmProviderAppConfirm',
+      providerApp: '=',
+      action: '@',
+      confirmAction: '&',
+      confirmTitle: '@',
+      cancel: '@'
+    },
+    templateUrl: "assets/wm_provider_app_confirm.html",
+    link: function(scope, element, attrs) {
+
+      var initialButton = angular.element(element.children()[0]);
+      var message = angular.element(element.children()[1]);
+      var confirmButton = angular.element(message.children()[1]);
+      var cancelButton = angular.element(message.children()[2]);
+
+      initialButton.bind('click', open);
+      confirmButton.bind('click', close);
+      cancelButton.bind('click', close);
+
+      function open() {
+        message.addClass('js_show');
+        message.removeClass('js_hide');
+        initialButton.addClass('active');
+        initialButton.attr("disabled", "true");
+      }
+
+      function close() {
+        message.addClass('js_hide');
+        message.removeClass('js_show');
+        initialButton.removeClass('active');
+        initialButton.removeAttr("disabled");
+      }
+    }
+  }
+});
